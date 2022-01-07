e34c2bbf82f415ee60670eddd52a2c88dcf1f889
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-07 15:11:51 +0530

    UPDATED:for exceptions all test cases

---
+++ src/main/java/com/userRegistrationwithunitJ/PhoneNumberException.java
@@ -0,0 +1,13 @@
+package com.userRegistrationwithunitJ;
+
+public class PhoneNumberException extends Exception {
+		enum ExceptionType3 {
+			ENTERED_NULL,ENTERED_EMPTY;
+		}
+		ExceptionType3 type;
+		public PhoneNumberException(ExceptionType3 type,String message) {
+			super(message);
+			this.type = type;
+		}
+	}
+
---
+++ src/test/java/com/userRegistrationwithunitJ/AppTest.java
@@ -0,0 +1,20 @@
+package com.userRegistrationwithunitJ;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
