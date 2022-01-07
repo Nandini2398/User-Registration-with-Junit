f44592bb7538dc4a24501334c73549910152d561
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-07 15:31:01 +0530

    ADDED:last name validation

--- src/main/java/com/userRegistrationwithunitJ/UserDetails.java
+++ src/main/java/com/userRegistrationwithunitJ/UserDetails.java
@@ -1,20 +1,12 @@
 package com.userRegistrationwithunitJ;
 
-
 public class UserDetails {
-
 	private String firstName;
 	private String lastName;
-	private String email;
-	private String phoneNumber;
-	private String password;
-	public UserDetails(String firstName,String lastName,String email,String phoneNumber,String password) {
+	public UserDetails(String firstName,String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.email=email;
-		this.phoneNumber=phoneNumber;
-		this.password=password;
 	}
 	public String getFirstName() {
 		return firstName;
@@ -27,23 +19,5 @@ public class UserDetails {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
