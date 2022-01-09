6f2b9bdd2a8834116807e48bd205ab29c0572889
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-09 21:23:25 +0530

    UPDATED:to validate user entry by using lambda function

---
+++ src/main/java/com/userRegistrationwithunitJ/UserValidationException.java
@@ -0,0 +1,12 @@
+package com.userRegistrationwithunitJ;
+
+public class UserValidationException extends RuntimeException{
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY
+	}
+	ExceptionType type;
+	public UserValidationException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
