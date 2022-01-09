bcf1774964f0dc20eae527477928c55c959f26c3
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-09 14:11:41 +0530

    UPDATED:password having one upper case or not

--- src/test/java/com/userRegistrationwithunitJ/UserDetailsTest.java
+++ src/test/java/com/userRegistrationwithunitJ/UserDetailsTest.java
@@ -8,32 +8,32 @@ public class UserDetailsTest {
 
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		boolean isValid = register.validateFirstName("Nandini");
+		boolean isValid = register.validateFirstName("Ravi");
 		Assert.assertTrue(isValid);
 	}
 	@Test
 	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-		boolean isValid = register.validateFirstName("nandini");
+		boolean isValid = register.validateFirstName("ravi");
 		Assert.assertFalse(isValid);
 	}
 	@Test
 	public void givenFirstName_WhenShort_ShouldReturnFalse() {
-		boolean isValid = register.validateFirstName("na");
+		boolean isValid = register.validateFirstName("ra");
 		Assert.assertFalse(isValid);
 	}
 	@Test
 	public void givenLastName_WhenProper_ShouldReturnTrue() {
-		boolean isValid = register.validateLastName("Raj");
+		boolean isValid = register.validateLastName("Kumar");
 		Assert.assertTrue(isValid);
 	}
 	@Test
 	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
-		boolean isValid = register.validateLastName("raj");
+		boolean isValid = register.validateLastName("kumar");
 		Assert.assertFalse(isValid);
 	}
 	@Test
 	public void givenLastName_WhenShort_ShouldReturnFalse() {
-		boolean isValid = register.validateLastName("ra");
+		boolean isValid = register.validateLastName("ku");
 		Assert.assertFalse(isValid);
 	}
 	@Test
@@ -42,18 +42,33 @@ public class UserDetailsTest {
 		Assert.assertTrue(isValid);
 	}
 	@Test
-	public void givenEmail_When_ShouldReturnFalse() {
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
 		boolean isValid = register.validateEmail(".abc@gmail.com");
 		Assert.assertFalse(isValid);
 	}
 	@Test
-	public void givenPhoneNumber_When_ShouldReturnTrue() {
-		boolean isValid = register.validateEmail("91758460906");
+	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
+		boolean isValid = register.validateEmail("917620690906");
 		Assert.assertTrue(isValid);
 	}
 	@Test
-	public void givenPhoneNumber_When_ShouldReturnFalse() {
-		boolean isValid = register.validateEmail("8460");
+	public void givenPhoneNumber_WhenNotProper_ShouldReturnFalse() {
+		boolean isValid = register.validateEmail("5654");
+		Assert.assertFalse(isValid);
+	}
+	@Test
+	public void givenPassword_When1UpperCase_ShouldReturnTrue() {
+		boolean isValid = register.validateEmail("aBd5gd2@g");
+		Assert.assertTrue(isValid);
+	}
+	@Test
+	public void givenPassword_WhenProper_ShouldReturnTrue() {
+		boolean isValid = register.validateEmail("aBd5gd2@g");
+		Assert.assertTrue(isValid);
+	}
+	@Test
+	public void givenPassword_WhenNotProper_ShouldReturnFalse() {
+		boolean isValid = register.validateEmail("b3@fr");
 		Assert.assertFalse(isValid);
 	}
 }
